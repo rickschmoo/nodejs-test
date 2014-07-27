ae01c4857cde801eec73146fe7f7973c23b8fe08
Richard Boardman <rboardman@salesforce.com>
2014-07-26 22:26:09 -0700

    port fix

--- hello.js
+++ hello.js
@@ -9,8 +9,8 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-// var port = Number(process.env.PORT || 5000);
-var port = Number(4444);
+var port = Number(process.env.PORT || 5000);
+// var port = Number(4444);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
